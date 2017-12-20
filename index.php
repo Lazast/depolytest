7547fa9aeae4d164ee85fa3ea57de096240e6e7a
Lemine <84722742@qq.com>
2017-12-20 09:45:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 echo "<html>"
-echo "<title>V2<title>";
+echo "<title>V2</title>";
 echo "<body><h1>Hello, moto</h1>";
-echo "<h1>Hello, nokia</h1>";
+echo "<h1>Hello, nokia</h1>"
 echo "</body>";
 echo "</html>"
 ?>
